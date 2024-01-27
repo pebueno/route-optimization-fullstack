b3c3ced38a71e1f416d7e5bbb955244fc85d928c
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2024-01-27 17:37:36 -0300

    Create new route to use dijkstra

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const findShortestRoute = require('./dijkstra');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,7 +18,7 @@ app.get('/clients', async (req, res) => {
     if (search === undefined) search = '';
 
     const result = await pool.query(
-      "SELECT * FROM clients WHERE name || ' ' || email || telephone ILIKE $1",
+      "SELECT * FROM clients WHERE name || ' ' || email || telephone || x || y  ILIKE $1",
       [`%${search}%`]
     );
 
@@ -38,6 +39,24 @@ app.post('/clients', async (req, res) => {
     res.json(result.rows[0]);
   } catch (error) {
     console.error('Erro ao cadastrar cliente:', error);
+    res.status(500).send('Erro interno do servidor');
+  }
+});
+
+app.get('/bestroute', async (req, res) => {
+  try {
+
+    const result = await pool.query(
+      "SELECT * FROM clients;"
+    );
+
+    const Clients = result.rows;
+
+    // Encontra a rota mais curta.
+    const shortestRoute = findShortestRoute(Clients);
+    res.json(shortestRoute);
+  } catch (error) {
+    console.error('Erro ao obter clientes:', error);
     res.status(500).send('Erro interno do servidor');
   }
 });
